b79529abc1eb7d59bbf3541cb9867c7b640a9454
santoshsunuwarmukhiya <santoshsunuwarmukhiya@gmail.com>
2026-09-03 12:03:23 +0545

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Todo from './components/Todo'
 function App() {
 
   return (
-    <>
+    <div className={'w-96 mx-auto mt-24 p-4 border border-gray-200'}>
       <Todo />
-    </>
+    </div>
   )
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,25 +1,41 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import TodoItem from "./TodoItem";
 import { v4 as uuidv4 } from 'uuid';
 
 function Todo() {
     const [input, setInput] = useState("");
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(()=>{
+        const savedTodos = localStorage.getItem("todos");
+
+        return savedTodos ? JSON.parse(savedTodos) : [];
+    });
+    const [edit, setEdit] = useState(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (!input) return;
-        setTodos(preV => [...preV, {id:uuidv4(), text: input}]);
+        const inputTrimmed = input.trim();
+        if (inputTrimmed === '') return;
+        if(edit !== null){
+            setTodos(todos.map((todo)=>(todo.id === edit ? {...todo, text: inputTrimmed}: todo)));
+            setEdit(null);
+        }else{
+            setTodos(preV => [...preV, {id:uuidv4(), text: inputTrimmed}]);
+        }
+
         setInput("");
     }
+
+    useEffect(() => {
+       localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos])
   return (
     <>
-          <form onSubmit={handleSubmit}>
-              <input type="text" value={input} onChange={(e)=>setInput(e.target.value)} />
-              <button type="submit">Add</button>
+          <form onSubmit={handleSubmit} className={'border mb-2'}>
+              <input type="text" className={'p-2 rounded-2xl mr-2 focus:border-black-1'} value={input} onChange={(e)=>setInput(e.target.value)} />
+              <button type="submit" className={'py-2 px-6 rounded-2xl border-0 outline-0 cursor-pointer hover:bg-red-500 hover:text-white'}>{edit !== null ? "Update" : "Add"}</button>
           </form>
-        <ul>
-            <TodoItem todos={todos} setTodos={setTodos} />
+        <ul className={'list-unstyled text-2xl'}>
+            <TodoItem todos={todos} setTodos={setTodos} setEdit={setEdit} setInput={setInput} />
         </ul>
 
     </>
